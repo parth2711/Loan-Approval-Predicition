3b5688c26e399109af01e337ab7656fd8b16f75a
parth2711 <64655628+parth2711@users.noreply.github.com>
2025-12-28 22:00:55 +0530

    Extend pipeline with Decision Tree and Random Forest

--- main.py
+++ main.py
@@ -13,16 +13,29 @@ def load_data(file_path: pd.DataFrame)->pd.DataFrame:
     df=pd.read_csv(file_path)
     return df
 
-def data_scaling(X: pd.DataFrame):
-    scaler=StandardScaler()
-    X_scaled=scaler.fit_transform(X)
-    return X_scaled
+def feature_split(df: pd.DataFrame,target:str):
+    X=df.drop(target,axis=1)
+    y=df[target]
+    return X,y
 
-def data_split(X_scaled,y):
-    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
-    return X_train, X_test, y_train, y_test
+def data_split(X,y):
+    return train_test_split(X, y, test_size=0.2, random_state=42)
 
-def decision_tree(X_train):
-    dt_model=DecisionTreeClassifier()
-    dt_model.fit(X_train)
+def decision_tree(X_train,y_train):
+    dt_model=DecisionTreeClassifier(random_state=42)
+    dt_model.fit(X_train,y_train)
     return dt_model
+
+def random_forest(X_train,y_train):
+    rf_model=RandomForestClassifier(n_estimators=100,random_state=42)
+    rf_model.fit(X_train,y_train)
+    return rf_model
+
+def model_evaluation(model,X_test,y_test):
+    prediction=model.predict(X_test)
+    print(confusion_matrix(y_test,prediction))
+    print(classification_report(y_test,prediction))
+
+def save_model(model,model_name):
+    with open(f'{model}','wb') as f:
+        pickle.dump(model,f)
